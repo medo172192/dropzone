bff41c287baf6973e96613824ddc872d3042f6f8
mohamed hesham <ppick177@gmail.com>
2023-05-04 15:21:57 +0300

    upload dropzone

--- dropzone.js
+++ dropzone.js
@@ -1,4 +1,20 @@
- Dropzone.autoDiscover = false;
+<form class="form" action="#" method="POST" enctype="multipart/form-data">
+<div class="fv-row">
+    <div class="dropzone" id="kt_dropzonejs_example_1">
+        <div class="dz-message needsclick">
+            <i class="ki-duotone ki-file-up fs-3x text-primary"><span class="path1"></span><span
+                    class="path2"></span></i>
+            <div class="ms-4">
+                <h3 class="fs-5 fw-bold text-gray-900 mb-1">Drop files here or click to upload.</h3>
+                <span class="fs-7 fw-semibold text-gray-400">Upload files</span>
+            </div>
+        </div>
+    </div>
+</div>
+</form>
+
+
+Dropzone.autoDiscover = false;
         /*
          * __________________________________________________
          *             Upload multiple Images
